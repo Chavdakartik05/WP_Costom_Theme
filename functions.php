fe89ccaa71663f358f092c6b7bd45609d1390165
CHAVDA KARTIK <163974506+Chavdakartik05@users.noreply.github.com>
2024-11-05 15:49:58 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,8 +1,20 @@
 <?php
+// INCLUDE STYLESHEET AND SCRIPT FILE//
 function get_setup()
 {
     wp_enqueue_style('style',get_stylesheet_uri('style.css'));
     wp_enqueue_script("main",get_theme_file_uri('script.js'));
 }
 add_action('wp_enqueue_scripts','geet_setup');//hook
+
+//constome post widget//
+function get_custom()
+    {
+    add_theme_support('post-thumbnails');
+    add_theme_support('title-tag')    
+    }
+    add_action('after_setup_theme','get_custom');
+
+                    //CREATE MENU IN ADMIN DAPENAL(DASHBOARD)//
+    ragister_nav_menus(arry('primary-menu'=>'Main menu'))
 ?>
