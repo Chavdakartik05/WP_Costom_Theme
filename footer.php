b7f1aa99e943c2e62297ec4ea5d1c254f93bda59
CHAVDA KARTIK <163974506+Chavdakartik05@users.noreply.github.com>
2024-11-18 16:14:34 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p><b>&copy;All right's reserved by kartik chavda</b></p>
-</body>
-</html>
+<?php wp_footer(); ?>
+<footer>
+    <center>
+    <a href="">powerd by Bleach</a>
+    </center>
+</footer>
